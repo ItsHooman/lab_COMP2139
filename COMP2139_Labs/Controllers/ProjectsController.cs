94a0d7f5589a397d6ee0c4c4a9fbd63938c1c387
ItsHooman <152651728+ItsHooman@users.noreply.github.com>
2024-02-02 09:27:52 -0500

    lab 3

---
+++ COMP2139_Labs/Controllers/ProjectsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP2139_Labs.Models;
+
+namespace COMP2139_Labs.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" },
+                new Project { ProjectId = 2, Name = "Project 2", Description = "Second Project" },
+                new Project { ProjectId = 3, Name = "Project 3", Description = "Third Project" },
+                new Project { ProjectId = 4, Name = "Project 4", Description = "Fourth Project" }
+
+
+        };
+            return View(projects);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ COMP2139_Labs/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP2139_Labs.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string status { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
