dd606370558e05f203ce6e17191fdc06a9349a13
ItsHooman <152651728+ItsHooman@users.noreply.github.com>
2024-02-25 23:56:06 -0500

    Add files via upload

---
+++ CarRental.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GBC_TRAVEL.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using GBC_TRAVEL.Data;
+
+namespace GBC_TRAVEL.Controllers
+{
+    public class CarRentalController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CarRentalController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        // GET: CarRental
+        public IActionResult Index()
+        {
+            var carRentals = _db.CarRentals.ToList();
+            return View(carRentals);
+        }
+
+        // GET: CarRental/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carRental = _db.CarRentals.FirstOrDefault(m => m.CarRentalID == id);
+            if (carRental == null)
+            {
+                return NotFound();
+            }
+
+            return View(carRental);
+        }
+
+        // GET: CarRental/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CarRental/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("CarRentalID,CompanyName,Location,CarType,PricePerDay,PickupDate,DropoffDate")] CarRental carRental)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(carRental);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carRental);
+        }
+
+        // GET: CarRental/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carRental = _db.CarRentals.Find(id);
+            if (carRental == null)
+            {
+                return NotFound();
+            }
+            return View(carRental);
+        }
+
+        // POST: CarRental/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("CarRentalID,CompanyName,Location,CarType,PricePerDay,PickupDate,DropoffDate")] CarRental carRental)
+        {
+            if (id != carRental.CarRentalID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(carRental);
+                    _db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarRentalExists(carRental.CarRentalID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carRental);
+        }
+
+        // GET: CarRental/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carRental = _db.CarRentals.FirstOrDefault(m => m.CarRentalID == id);
+            if (carRental == null)
+            {
+                return NotFound();
+            }
+
+            return View(carRental);
+        }
+
+        // POST: CarRental/Delete/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var carRental = _db.CarRentals.Find(id);
+            _db.CarRentals.Remove(carRental);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CarRentalExists(int id)
+        {
+            return _db.CarRentals.Any(e => e.CarRentalID == id);
+        }
+    }
+}
---
+++ HotelController.cs
@@ -0,0 +1,150 @@
+﻿using GBC_TRAVEL.Data;
+using GBC_TRAVEL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace GBC_TRAVEL.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public HotelController(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        // GET: Hotels
+        public IActionResult Index()
+        {
+
+            var flights = _db.Hotels.ToList();
+            return View(flights);
+        }
+
+        // GET: Hotels/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = _db.Hotels.FirstOrDefault(m => m.HotelID == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // GET: Hotels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Hotels/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("HotelID,Name,Location,Rating,NumberOfRooms,PricePerNight")] Hotel hotel)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(hotel);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: Hotels/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = _db.Hotels.Find(id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+            return View(hotel);
+        }
+
+        // POST: Hotels/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("HotelID,Name,Location,Rating,NumberOfRooms,PricePerNight")] Hotel hotel)
+        {
+            if (id != hotel.HotelID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _db.Update(hotel);
+                    _db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HotelExists(hotel.HotelID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotel);
+        }
+
+        // GET: Hotels/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hotel = _db.Hotels.FirstOrDefault(m => m.HotelID == id);
+            if (hotel == null)
+            {
+                return NotFound();
+            }
+
+            return View(hotel);
+        }
+
+        // POST: Hotels/Delete/5
+        [HttpPost, ActionName("DeleteConfirmed")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var hotel = _db.Hotels.Find(id);
+            if(hotel != null)
+            {
+                _db.Hotels.Remove(hotel);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+            
+
+        private bool HotelExists(int id)
+        {
+            return _db.Hotels.Any(e => e.HotelID == id);
+        }
+    }  
+}
