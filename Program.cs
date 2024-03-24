5e5b6772cde831f5b178bc59680dbf49c4dc2484
ItsHooman <152651728+ItsHooman@users.noreply.github.com>
2024-03-23 23:23:00 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 using Microsoft.EntityFrameworkCore;
-using GBC_TRAVEL.Data;
+using WebApplication11.Data;
+using Microsoft.AspNetCore.Identity;
+using WebApplication11.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnenction")));
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
 
+builder.Services.AddSingleton<IEmailSender, EmailSender>(); // Hey Microsoft if a class has input constructor  from IEmail sender , make an object from email sender and inject it
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //Lab 5 - custom Error page
+    app.UseStatusCodePagesWithRedirects("/Home/NotFound?statusCode={0}");
     app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
@@ -24,10 +37,17 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();    
 app.UseAuthorization();
+app.MapRazorPages();
 
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Projects}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
